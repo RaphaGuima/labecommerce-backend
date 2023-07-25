57d65d40d343c59eb8b7b06f43e1c1262fa9a8a2
rapha <raphaelgui27@gmail.com>
2023-07-25 09:50:23 -0300

    exerc typescript II finalizado

--- build/index.js
+++ build/index.js
@@ -1,3 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("testantando as atividades de typescript-1");
+const database_1 = require("./database");
+function testImports() {
+    console.log("Users:");
+    console.log(database_1.users);
+    console.log("Products:");
+    console.log(database_1.products);
+    console.log("Data de criação:");
+    console.log(database_1.data2);
+}
+//testImports()
+console.log(database_1.allUsers, database_1.creatUser);
