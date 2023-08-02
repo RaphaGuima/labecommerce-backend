7b15e693bbb16c5b16b4b4c505114ea5ba44a90c
rapha <raphaelgui27@gmail.com>
2023-08-02 13:36:53 -0300

    exercicios de apis-e-express feitos

--- build/index.js
+++ build/index.js
@@ -1,6 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003");
+});
+app.get("/ping", (req, res) => {
+    res.send("Pong!");
+});
 function testImports() {
     console.log("Users:");
     console.log(database_1.users);
@@ -10,4 +24,3 @@ function testImports() {
     console.log(database_1.data2);
 }
 //testImports()
-console.log(database_1.allUsers, database_1.creatUser);
